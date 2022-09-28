4e132c32db88299f4930e504b48eece38d061131
RodrigoGama <rodrigo1902.rb@gmail.com>
2022-09-28 15:45:26 -0300

    Added license block

--- __init__.py
+++ __init__.py
@@ -1,3 +1,23 @@
+# "AutoFakeUser" Blender Addon.
+# Copyright (C) 2022, Rodrigo Gama
+#
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 bl_info = {
     "name": "Auto Fake User",
     "description": "Automatically set Fake User to enabled data blocks when saving",
