320296eb90d18568967feae6808dc384f8d072dc
RodrigoGama <rodrigo1902.rb@gmail.com>
2022-09-28 15:43:13 -0300

    Version Update

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Auto Fake User",
     "description": "Automatically set Fake User to enabled data blocks when saving",
     "author": "Rodrigo Gama",
-    "version": (0, 0, 1),
+    "version": (0, 1, 0),
     "blender": (3, 2, 0),
     "location": "View3D",
     "category": "3D View"}
